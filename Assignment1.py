dac88d78ed2ec23265c1bd6db33d94c8be342f5b
Aalandikar <aalandikar@gmail.com>
2024-04-04 20:55:45 +0530

    first commit

---
+++ Assignment1.py
@@ -0,0 +1,10 @@
+name="Ankita Alandikar"
+age=28
+city="Pune"
+email="aalandikar@gmail.com"
+phoneNumber=8329823269
+print("Name :" ,name)
+print("Age :" ,age)
+print("Email : ",email)
+print("City :" ,city)
+print("Phone Number :",phoneNumber)
